e222cc98cf8fb34b49aab2916fbc51b1bed8bce8
Barry Bell <barry@contention.agency>
2025-01-10 16:43:21 +0000

    Work on building html

--- app/createhtml.py
+++ app/createhtml.py
@@ -3,9 +3,7 @@ import time
 import urllib.request
 import json
 
-SECTION="world"
-
-FILENAME="The Guardian | " + str(time.strftime("%Y-%m-%d %H:%M:%S") + ".html")
+FILENAME="TheGuardian.html"
 
 # Function to create an HTML file
 def createhtml():
@@ -13,25 +11,61 @@ def createhtml():
     # Open a file
     file = open("/output/" + str(FILENAME), "w")
 
-    # Write HTML content
+    # Write the opening HTML tags
     file.write("<html>")
     file.write("<head>")
     file.write("<title>The Guardian</title>")
     file.write("</head>")
     file.write("<body>")
 
-    # Get data from the Guardian API
-    apiurl = "https://content.guardianapis.com/search?section=" + str(SECTION) + "&type=article&show-fields=all&show-blocks=body&page-size=25&shouldHideAdverts=true&api-key=" + str(os.environ.get("GUARDIAN_API_KEY"))
-    print(apiurl)
-    with urllib.request.urlopen(apiurl) as url:
-        data = json.load(url)
+    toc_string = ""
+    content_string = ""
 
-    print(data["response"]["status"])
-        
-    for article in data["response"]["results"]:
-        file.write("<h1>"+ str(article["fields"]["headline"])+"</h1>")
-        file.write(str(article["fields"]["body"]))
+
+    with open('app/sources.json') as fp:
+        sources = json.load(fp)
+
+    for source in sources:
+        title = source["title"]
+
+        toc_string += "<li>"+str(title)+"</li>"
+        content_string += "<h1 id='"+str(title)+"'>"+ str(title)+"</h1>"
+
+        for section in source["sections"]:
+            section_id = section["id"]
+            section_title = section["title"]
+
+            # Get data from the Guardian API
+            print("Fetching data from the Guardian API for section: " + str(section_id))
+            apiurl = "https://content.guardianapis.com/search?section=" + str(section_id) + "&type=article&show-fields=all&show-blocks=body&page-size=25&shouldHideAdverts=true&api-key=" + str(os.environ.get("GUARDIAN_API_KEY"))
+
+            with urllib.request.urlopen(apiurl) as url:
+                data = json.load(url)
+
+            if data["response"]["status"] == "ok":
+                toc_string += "<li><a href='#"+str(section_title)+"'>"+str(section_title)+"</a></li>"
+                content_string += "<h2>"+ str(section_title)+"</h2>"
+                for article in data["response"]["results"]:
+
+                    toc_string += "<li><a href='#"+article["id"]+"'>"+str(article["fields"]["headline"])+"</a></li>"
+                    
+                    content_string += "<h3 id='"+article["id"]+"'>"+ str(article["fields"]["headline"])+"</h3>"
+                    content_string +=  str(article["fields"]["body"])
+            else:
+                content_string += "<h3>Error fetching the '"+section_title+"' section!</h3>"
+
+            time.sleep(1)
+
+    # Write the Table of Contents
+    file.write("<h1>Table of Contents</h1>")
+    file.write("<ul>")
+    file.write(toc_string)
+    file.write("</ul>")
     
+    # Write the content
+    file.write(content_string)
+
+    # Write the closing tags
     file.write("</body>")
     file.write("</html>")
 
