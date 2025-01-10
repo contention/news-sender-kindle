2892e6a7e0b57b1d50f69443752dd53a0864b8b5
Barry Bell <barry@contention.agency>
2025-01-10 15:02:48 +0000

    Work on function

--- app/createhtml.py
+++ app/createhtml.py
@@ -1,21 +1,37 @@
-import urllib.request, json
+import os
+import time
+import urllib.request
+import json
 
+SECTION="world"
+
+FILENAME="The Guardian | " + str(time.strftime("%Y-%m-%d %H:%M:%S") + ".html")
+
+# Function to create an HTML file
 def createhtml():
 
-
-    with urllib.request.urlopen("http://maps.googleapis.com/maps/api/geocode/json?address=google") as url:
-        data = json.load(url)
-        print(data)
-
-    file = open("output.html", "w")
+    # Open a file
+    file = open("/output/" + str(FILENAME), "w")
 
     # Write HTML content
     file.write("<html>")
     file.write("<head>")
-    file.write("<title>My Webpage</title>")
+    file.write("<title>The Guardian</title>")
     file.write("</head>")
     file.write("<body>")
-    file.write("<h1>Welcome to my webpage!</h1>")
+
+    # Get data from the Guardian API
+    apiurl = "https://content.guardianapis.com/search?section=" + str(SECTION) + "&type=article&show-fields=all&show-blocks=body&page-size=25&shouldHideAdverts=true&api-key=" + str(os.environ.get("GUARDIAN_API_KEY"))
+    print(apiurl)
+    with urllib.request.urlopen(apiurl) as url:
+        data = json.load(url)
+
+    print(data["response"]["status"])
+        
+    for article in data["response"]["results"]:
+        file.write("<h1>"+ str(article["fields"]["headline"])+"</h1>")
+        file.write(str(article["fields"]["body"]))
+    
     file.write("</body>")
     file.write("</html>")
 
