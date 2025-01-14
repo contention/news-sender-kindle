ce9350be45a7da64f70f06e8902e0d358c00502a
Barry Bell <barry@contention.agency>
2025-01-14 16:59:25 +0000

    Work on flaskerizing

--- app/app.py
+++ app/app.py
@@ -1,11 +1,17 @@
-from flask import Flask
+import flask
 import os
 
-app = Flask(__name__)
+app = flask.Flask(__name__, static_folder='static', static_url_path='')
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    if flask.request.method == 'POST':
+        if 'password' in list(flask.request.form):
+            if flask.request.form['password'] == os.environ.get("PASSWORD"):
+                return flask.render_template('build.html')
+            else:
+                return flask.render_template('index.html')
+    return flask.render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=os.environ.get("LOCAL_PORT", 5000), debug=True)
+    app.run(host="0.0.0.0", port=os.environ.get("LOCAL_PORT", 5000), debug=os.environ.get("DEBUG", False), threaded=True)
