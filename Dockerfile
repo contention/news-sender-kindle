48a34c0a2b47ea5905f297b98e816ee8c29988e7
Barry Bell <barry@contention.agency>
2025-01-16 17:42:33 +0000

    Work on better reporting

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM python:3.10
 
-RUN apt-get update && apt-get install -y calibre && pip3 install pillow pytz flask
+RUN apt-get update && apt-get install -y calibre && pip3 install pillow pytz flask flask-cors
 
 RUN mkdir -p /output
 RUN chmod 777 /output
--- app/app.py
+++ app/app.py
@@ -1,9 +1,13 @@
 import flask
 import os
+import threading
 from scripts.build import build
+from flask_cors import CORS
+from time import sleep
 
 app = flask.Flask(__name__, static_folder='static', static_url_path='')
 app.secret_key = os.environ.get("SECRET_KEY")
+CORS(app)
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
@@ -11,7 +15,8 @@ def index():
         if 'password' in list(flask.request.form):
             if flask.request.form['password'] == os.environ.get("PASSWORD"):
                 flask.session['authenticated'] = True
-                return flask.render_template('output.html')
+                #Launch build here
+                return flask.render_template('status.html')
             else:
                 flask.session['authenticated'] = False
                 return flask.render_template('error.html')
@@ -29,6 +34,17 @@ def dobuild():
         return app.response_class(build(), mimetype="text/plain")
 
 
+@app.route("/status", methods=['GET', 'POST'])
+def checkstatus():
+    if os.path.isfile("/output/status.txt"):
+        f = open("/output/status.txt", "r")
+        statustext = f.read()
+        f.close()
+        return app.response_class(statustext, mimetype="text/plain")
+        
+    else:
+        return app.response_class("No status", mimetype="text/plain")
+    
 
 
 if __name__ == "__main__":
